a426c8fe97a3616da2c53c18c916f0154cac2d7a
Mauricio <Mauricio1932>
2022-01-20 19:10:40 -0600

    modelo primer y segundo modelo

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #componentes creados
+    'primerComponente',
+
+    #librerias Agregadas
     'rest_framework',
 ]
 
---
+++ primerComponente/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+
+# Create your models here.
+class primerModelo(models.Model):
+    campo_uno = models.CharField(max_length=255, null=True)
+    edad = models.IntegerField(null=True, default=0)
+    created = models.DateTimeField(default=timezone.now)
+    edit = models.DateTimeField(blank=True, null=True, default=None)
+    
+class SegundoModelo(models.Model):
+    campo_uno = models.CharField(max_length=255, null=True)
+    edad = models.IntegerField(null=True, default=0)
+    created = models.DateTimeField(default=timezone.now)
+    edit = models.DateTimeField(blank=True, null=True, default=None)
+
+    class Meta:
+      db_table = 'segundo_modelo'
